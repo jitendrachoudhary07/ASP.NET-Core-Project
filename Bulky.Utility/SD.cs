06a6da2a6074776edb8c417ee946d0f675f78583
Jitendra Choudhary <jitendrachoudhary7087@gmail.com>
2024-06-11 18:56:31 +0530

    Add files via upload

---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Utility
+{
+    public static class SD
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+        /*public static string Role_Customer { get; set; }
+        public static string Role_Employee { get; set; }
+        public static string Role_Admin { get; set; }
+        public static string Role_Company { get; set; }*/
+    }
+}
---
+++ demolibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace demolibrary
+{
+    public class Class1
+    {
+        public int x; public int y;
+        public void func1(int x)
+        {
+
+        }
+    }
+}
